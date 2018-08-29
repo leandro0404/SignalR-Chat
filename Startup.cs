d1f202d69dbc954dc6fc238b6a108c6b5552c656
Leandro Silveira Alves Moreira <leandrobhmgg@gmail.com>
2018-08-29 00:09:06 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+
+           
+        }
+
+      
+    }
+}
